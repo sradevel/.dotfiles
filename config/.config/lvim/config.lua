d618f738666a31b8d5c82233f948e9996e4d4afc
sraabe <sradevelgmx.de>
2021-11-28 23:57:47 +0100

    refactored lvim.config, added CMake which-key bindings, added codelldb config

--- config/.config/lvim/config.lua
+++ config/.config/lvim/config.lua
@@ -11,49 +11,22 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
---lvim.colorscheme = "onedarker"
 lvim.colorscheme = "gruvbox-material"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
+
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.term_mode["<Esc>"] = "<C-\\><C-n>"
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
-
--- lvim.lang.c.formatters = {{exe = "clang-format"}, args = {"--style=Microsoft", "-i"}}
--- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
--- }
+-- (N)Vim options
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
@@ -81,56 +54,16 @@ lvim.builtin.treesitter.ensure_installed = {
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
--- you can set a custom on_attach function that will be used for all the language servers
--- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
--- you can overwrite the null_ls setup table (useful for setting the root_dir function)
--- lvim.lsp.null_ls.setup = {
---   root_dir = require("lspconfig").util.root_pattern("Makefile", ".git", "node_modules"),
--- }
--- or if you need something more advanced
--- lvim.lsp.null_ls.setup.root_dir = function(fname)
---   if vim.bo.filetype == "javascript" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "node_modules")(fname)
---       or require("lspconfig/util").path.dirname(fname)
---   elseif vim.bo.filetype == "php" then
---     return require("lspconfig/util").root_pattern("Makefile", ".git", "composer.json")(fname) or vim.fn.getcwd()
---   else
---     return require("lspconfig/util").root_pattern("Makefile", ".git")(fname) or require("lspconfig/util").path.dirname(fname)
---   end
--- end
-
--- set a formatter if you want to override the default lsp one (if it exists)
--- lvim.lang.python.formatters = {
---   {
---     exe = "black",
---   }
--- }
--- set an additional linter
--- lvim.lang.python.linters = {
---   {
---     exe = "flake8",
---   }
--- }
-
 -- Additional Plugins
 lvim.plugins = {
---     {"folke/tokyonight.nvim"},
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
    {"sainnhe/gruvbox-material"},
-    {"shatur/neovim-cmake"},
-    {"mfussenegger/nvim-dap"},
- }
+   {"shatur/neovim-cmake"},
+   {"theHamsta/nvim-dap-virtual-text",
+    config = function()
+        require("nvim-dap-virtual-text").setup()
+        end,
+    },
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
@@ -146,10 +79,112 @@ if ok then
     end
 end
 
--- (N)Vim options
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+
+local dap = require('dap')
+dap.adapters.codelldb = function(on_adapter)
+  local stdout = vim.loop.new_pipe(false)
+  local stderr = vim.loop.new_pipe(false)
+
+  -- CHANGE THIS!
+  local home = os.getenv("HOME");
+  local cmd = home .. '/.local/share/nvim/dapinstall/codelldb/extension/adapter/codelldb'
+  local handle, pid_or_err
+  local opts = {
+    stdio = {nil, stdout, stderr},
+    detached = true,
+  }
+  handle, pid_or_err = vim.loop.spawn(cmd, opts, function(code)
+    stdout:close()
+    stderr:close()
+    handle:close()
+    if code ~= 0 then
+      print("codelldb exited with code", code)
+    end
+  end)
+  assert(handle, "Error running codelldb: " .. tostring(pid_or_err))
+  stdout:read_start(function(err, chunk)
+    assert(not err, err)
+    if chunk then
+      local port = chunk:match('Listening on port (%d+)')
+      if port then
+        vim.schedule(function()
+          on_adapter({
+            type = 'server',
+            host = '127.0.0.1',
+            port = port
+          })
+        end)
+      else
+        vim.schedule(function()
+          require("dap.repl").append(chunk)
+        end)
+      end
+    end
+  end)
+  stderr:read_start(function(err, chunk)
+    assert(not err, err)
+    if chunk then
+      vim.schedule(function()
+        require("dap.repl").append(chunk)
+      end)
+    end
+  end)
+end
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  }
+}
+dap.configurations.c = dap.configurations.cpp
+
+-- CMake Whickkey Menu
+lvim.builtin.which_key.mappings["C"] = {
+    name = "CMake",
+    s = { "<cmd>:Telescope cmake select_target<CR>", "Select target"},
+    b = { "<cmd>:CMake build<CR>", "Build"},
+    a = { "<cmd>:CMake build_all<CR>", "Build all"},
+    c = { "<cmd>:CMake configure<CR>", "Configure project"},
+    r = { "<cmd>:CMake run<CR>", "Run selected target"},
+    d = { "<cmd>:CMake debug<CR>", "Debug selected target"},
+    l = { "<cmd>:CMake clean<CR>", "Clean target"},
+}
+
+require('cmake').setup({
+  dap_configuration = {
+    type = 'codelldb',
+    request = 'launch',
+    stopOnEntry = false,
+    runInTerminal = false,
+}})
+
+-- Debug Whichkey Menu
+-- lvim.builtin.which_key.mappings["d"] = {
+--   name = "Debug",
+--   b = { "<cmd>:lua require'dap'.toggle_breakpoint()<CR>", "Toggle Breakpoint" },
+lvim.builtin.which_key.mappings["dz"] = { "<cmd>:lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", "Conditional Breakpoint" }
+lvim.builtin.which_key.mappings["dy"] = { "<cmd>:lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>", "Log Breakpoint" }
+--   r = { "<cmd>:lua require'dap'.continue()<CR>", "Run/Continue (<F5>)" },
+--   u = { "<cmd>:lua require'dap'.step_over()<CR>", "Step over (<F10>)" },
+--   i = { "<cmd>:lua require'dap'.step_into()<CR>", "Step into (<F11>)" },
+--   o = { "<cmd>:lua require'dap'.step_out()<CR>", "Step out (<F12>)" },
+--   p = { "<cmd>:lua require'dap'.repl_open()<CR>", "Open Repl" },
+--   a = { "<cmd>:lua require'dap'.run_last()<CR>", "Run last" },
+-- }
+
+-- Keybindings for nvim-dap
+vim.cmd(
+    [[
+     nnoremap <silent> <F5> :lua require'dap'.continue()<CR>
+     nnoremap <silent> <F10> :lua require'dap'.step_over()<CR>
+     nnoremap <silent> <F11> :lua require'dap'.step_into()<CR>
+     nnoremap <silent> <F12> :lua require'dap'.step_out()<CR>
+    ]]
+)
