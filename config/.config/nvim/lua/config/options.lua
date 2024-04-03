eea074ab1c0a6029fd970d654f9462d523214af1
sraabe <sradevelgmx.de>
2024-04-03 17:08:29 +0200

    updated lazy, added filetype for ssh configs in 'config.d'

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -2,3 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.spelllang = "en,de"
+
+-- additional filetype mappings
+vim.filetype.add({
+  -- I've organized my sshconfigs in a
+  -- config.d subdirectory
+  pattern = {
+    [".*.ssh/config.d/.*"] = "sshconfig",
+  },
+})
